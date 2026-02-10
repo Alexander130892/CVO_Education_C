07bca178208180c6765d9144fdf911cf3e218767
Alexander130892 <168905121+Alexander130892@users.noreply.github.com>
2026-02-10 19:09:09 +0100

    Effect of non-zero booleans

---
+++ Lesson2_Types+Arrays+Sorting/Exercise_3.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    bool e = 6;
+    bool f = -5;
+    printf("Waarde van a: %d\n", e);
+    printf("Waarde van b: %d\n", f);
+    
+    // --> Any non-zero value = true
+
+    return EXIT_SUCCESS;
+}
