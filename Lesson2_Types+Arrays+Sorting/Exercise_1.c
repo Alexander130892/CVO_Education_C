871d1e906195fd1326fbbcc24e2f53f9c4554a67
Alexander130892 <168905121+Alexander130892@users.noreply.github.com>
2026-02-10 18:52:08 +0100

    Chars and ASCII table excercise

---
+++ Lesson2_Types+Arrays+Sorting/Exercise_1.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    char c = 65;
+    printf("Original character: %c\n", c);
+    printf("New character: %c\n", c+32);
+    return EXIT_SUCCESS;
+}
