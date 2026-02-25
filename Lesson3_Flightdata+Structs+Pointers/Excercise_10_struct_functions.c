d6f6bf0ecbc79c54b2d71976a2640df919b5a53b
Alexander130892 <168905121+Alexander130892@users.noreply.github.com>
2026-02-25 20:45:00 +0100

    structs functions, -> operand

---
+++ Lesson3_Flightdata+Structs+Pointers/Excercise_10_struct_functions.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    char* title;
+    char* author;
+    float price;
+}Book;
+
+void printInfo(Book*);
+
+int main(){
+    Book books[3]={
+        {"Lord of the Rings", "J.R.R. Tolkien", 19.99},
+        {"1984", "George Orwell", 10.99},
+        {"Mythos", "Stephen Fry", 15.99}
+    };
+
+    printf("Here are 3 books: \n");
+    for(int i=0;i<3;i++){
+        printInfo(&books[i]);
+    }
+    return EXIT_SUCCESS;
+}
+
+void printInfo(Book* book){
+    printf("%s by %s, Price: $%.2f\n", book->title, book->author, (*book).price);
+
+}
